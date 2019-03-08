de0a9acfea9acea9f50225c5dc14a9edc7aa6785
yp <00000000>
2019-03-08 10:36:47 +0800

    customer export

--- src/main/java/com/zhongmei/yunfu/controller/model/CustomerDrainSearchModel.kt
+++ src/main/java/com/zhongmei/yunfu/controller/model/CustomerDrainSearchModel.kt
@@ -5,7 +5,7 @@ import java.math.BigDecimal
 
 class CustomerDrainSearchModel : WebBaseModel() {
 
-    var opType: String? = null
+    var opType: Int? = null
     var consumptionLastTime: String? = null
     var storedBalance: BigDecimal? = null
     var cardResidueCount: Int? = null
--- src/main/java/com/zhongmei/yunfu/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/zhongmei/yunfu/service/impl/CustomerServiceImpl.java
@@ -165,8 +165,13 @@ public class CustomerServiceImpl extends BaseServiceImpl<CustomerMapper, Custome
         customerEntity.setShopIdenty(searchModel.getUser().getShopIdenty());
         EntityWrapperFilter eWrapper = new EntityWrapperFilter<>(customerEntity);
         //eWrapper.eq("relate_id", 0L);
-        eWrapper.gt("stored_balance", searchModel.getStoredBalance());
-        eWrapper.gt("card_residue_count", searchModel.getCardResidueCount());
+        if (searchModel.getOpType() == null || searchModel.getOpType() == 1) {
+            eWrapper.ge("stored_balance", searchModel.getStoredBalance());
+            eWrapper.ge("card_residue_count", searchModel.getCardResidueCount());
+        } else {
+            eWrapper.le("stored_balance", searchModel.getStoredBalance());
+            eWrapper.le("card_residue_count", searchModel.getCardResidueCount());
+        }
         eWrapper.gt("consumption_last_time", searchModel.getConsumptionLastTime());
         Page<CustomerEntity> page = new Page<>(searchModel.getPageNo(), searchModel.getPageSize());
         return selectPage(page, eWrapper);
