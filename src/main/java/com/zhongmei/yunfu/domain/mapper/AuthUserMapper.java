0d5238c6d94d5e33f670206aa8859d21c2e38b0a
YorkZhao <zhaos@shishike.com>
2019-04-18 21:25:13 +0800

    添加会员到店报表

--- src/main/java/com/zhongmei/yunfu/domain/mapper/AuthUserMapper.java
+++ src/main/java/com/zhongmei/yunfu/domain/mapper/AuthUserMapper.java
@@ -51,14 +51,35 @@ public interface AuthUserMapper extends BaseMapper<AuthUserEntity> {
             "  AND au.shop_identy = #{shopId}")
     List<AuthPermissionEntity> getAuthPermissionEntity(@Param("authUserId") Long authUserId, @Param("shopId") Long shopId);
 
-    @Select("SELECT tu.`user_name` as userName , sum(ti.`actual_amount`) as amount, count(ti.`id`)  as count FROM  `trade_user`  tu LEFT JOIN `trade_item` ti on tu.`trade_item_id` = ti.`id` LEFT JOIN `trade` t on ti.`trade_id` = t.`id` \n" +
-            "${ew.sqlSegment} \n" +
-            "GROUP BY tu.`user_id` \n" +
-            "ORDER BY sum(ti.`actual_amount`) desc; ")
+//    @Select("SELECT tu.`user_name` as userName , sum(ti.`actual_amount`) as amount, count(ti.`id`)  as count FROM  `trade_user`  tu LEFT JOIN `trade_item` ti on tu.`trade_item_id` = ti.`id` LEFT JOIN `trade` t on ti.`trade_id` = t.`id` \n" +
+//            "${ew.sqlSegment} \n" +
+//            "GROUP BY tu.`user_id` \n" +
+//            "ORDER BY sum(ti.`actual_amount`) desc; ")
+
+    @Select("SELECT sum(a.amount) as amount ,a.name as userName, count(1) as count from(\n" +
+            "SELECT t.`trade_amount`as amount,  t.`trade_time` as time , tu.`user_name` as name,tu.`user_id` as userId, t.`business_type` as type  FROM `trade_user` tu LEFT JOIN `trade` t on t.`id` = tu.`trade_id` \n" +
+            "WHERE tu.`trade_item_id` IS NULL ${ew.sqlSegment}\n" +
+            "union\n" +
+            "SELECT ti.`actual_amount` as amount, ti.`server_create_time`as time, tu.`user_name` as name , tu.`user_id`as userId, t.`business_type` as type    FROM `trade_user` tu LEFT JOIN `trade_item` ti on tu.`trade_item_id` = ti.`id` LEFT JOIN `trade` t on ti.`trade_id` = t.`id` \n" +
+            "WHERE tu.`trade_item_id` IS NOT NULL ${ew.sqlSegment} \n" +
+            "ORDER BY time desc) a\n" +
+            "GROUP BY a.userId\n" +
+            "ORDER BY amount desc;")
     List<UserSalaryReport> querUserSaleryReport(@Param("ew") Condition wrapper);
 
-    @Select("SELECT t.business_type as businessType,tu.`user_name` as userName , ti.`actual_amount`  as amount, ti.`dish_name` as dishName, ti.`server_create_time` as tradeDate FROM  `trade_user`  tu LEFT JOIN `trade_item` ti on tu.`trade_item_id` = ti.`id` LEFT JOIN `trade` t on ti.`trade_id` = t.`id`  \n" +
-            "${ew.sqlSegment} \n" +
-            "ORDER BY ti.`server_create_time` desc; ")
+//    @Select("SELECT t.business_type as businessType,tu.`user_name` as userName , ti.`actual_amount`  as amount, ti.`dish_name` as dishName, ti.`server_create_time` as tradeDate FROM  `trade_user`  tu LEFT JOIN `trade_item` ti on tu.`trade_item_id` = ti.`id` LEFT JOIN `trade` t on ti.`trade_id` = t.`id`  \n" +
+//            "${ew.sqlSegment} \n" +
+//            "ORDER BY ti.`server_create_time` desc; ")
+
+    @Select("SELECT a.type as businessType,a.name as userName,a.amount as amount,a.dishName as dishName,a.time as tradeDate\n" +
+            "from (\n" +
+            "SELECT t.`trade_amount`as amount,  t.`trade_time` as time , tu.`user_name` as name,tu.`user_id` as userId, t.`business_type` as type , '--整单服务--' as dishName\n" +
+            "FROM `trade_user` tu LEFT JOIN `trade` t on t.`id` = tu.`trade_id` \n" +
+            "WHERE tu.`trade_item_id` IS NULL ${ew.sqlSegment} \n" +
+            "union\n" +
+            "SELECT ti.`actual_amount` as amount, ti.`server_create_time`as time, tu.`user_name` as name , tu.`user_id`as userId, t.`business_type` as type,ti.`dish_name` as dishName\n" +
+            "FROM `trade_user` tu LEFT JOIN `trade_item` ti on tu.`trade_item_id` = ti.`id` LEFT JOIN `trade` t on ti.`trade_id` = t.`id` \n" +
+            "WHERE tu.`trade_item_id` IS NOT NULL ${ew.sqlSegment} \n" +
+            "ORDER BY time desc ) a")
     List<UserSalaryReport> querUserSaleryDetailReport(@Param("ew") Condition wrapper);
 }
