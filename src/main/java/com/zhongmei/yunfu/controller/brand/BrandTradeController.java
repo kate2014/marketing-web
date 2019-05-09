05a5ecced61c46e78ea16369cf4c2f36d1717fff
YorkZhao <zhaos@shishike.com>
2019-05-09 16:43:29 +0800

    增加品牌管理报表

---
+++ src/main/java/com/zhongmei/yunfu/controller/brand/BrandTradeController.java
@@ -0,0 +1,208 @@
+package com.zhongmei.yunfu.controller.brand;
+
+
+import com.baomidou.mybatisplus.plugins.Page;
+import com.zhongmei.yunfu.controller.BaseController;
+import com.zhongmei.yunfu.controller.model.TradeItemModel;
+import com.zhongmei.yunfu.controller.model.TradeModel;
+import com.zhongmei.yunfu.domain.entity.*;
+import com.zhongmei.yunfu.service.*;
+import com.zhongmei.yunfu.util.DateFormatUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.*;
+
+/**
+ * <p>
+ * 交易记录主单（相当于ORDERS）。
+ * 主单及其所有子单中的数量、金额在退货时都取反 前端控制器
+ * </p>
+ *
+ * @author yangyp
+ * @since 2018-09-17
+ */
+@Controller
+@RequestMapping("/internal/brand/trade")
+public class BrandTradeController extends BaseController{
+
+    @Autowired
+    TradeService mTradeService;
+    @Autowired
+    TradeItemService mTradeItemService;
+    @Autowired
+    PaymentItemService mPaymentItemService;
+    @Autowired
+    TradePrivilegeService mTradePrivilegeService;
+    @Autowired
+    TradeUserService mTradeUserService;
+    @Autowired
+    TradeCustomerService mTradeCustomerService;
+
+    @RequestMapping("/listData")
+    public String tradeListPage(Model model, TradeModel mTradeModel) {
+
+        try {
+            //设置默认查询时间
+            if (mTradeModel.getStartDate() == null) {
+                Calendar c = Calendar.getInstance();
+                //过去15天
+                c.setTime(new Date());
+                c.add(Calendar.DATE, -15);
+                Date start = c.getTime();
+                String temp = DateFormatUtil.format(start, DateFormatUtil.FORMAT_FULL_DATE);
+                start = DateFormatUtil.parseDate(temp, DateFormatUtil.FORMAT_FULL_DATE);
+
+                mTradeModel.setStartDate(temp);
+            }
+            if (mTradeModel.getEndDate() == null) {
+                Date end = new Date();
+                mTradeModel.setEndDate(DateFormatUtil.format(end, DateFormatUtil.FORMAT_FULL_DATE));
+            }
+
+            Page<TradeEntity> listTrade = mTradeService.queryListTradePage(mTradeModel);
+            setWebPage(model, "/internal/trade/listData", listTrade, mTradeModel);
+            model.addAttribute("mTradeModel", mTradeModel);
+            model.addAttribute("listData", listTrade.getRecords());
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return "fail";
+
+        }
+
+        return "brand_trade_list";
+    }
+
+    @RequestMapping("/tradeDetail")
+    public String tradeDetail(Model model, TradeModel mTradeModel){
+
+        try{
+            TradeEntity mTradeEntity = mTradeService.queryTradeById(mTradeModel.getTradeId());
+            model.addAttribute("mTradeEntity", mTradeEntity);
+
+            List<TradeItemEntity> listItem = mTradeItemService.querTradeItemByTradeId(mTradeEntity.getId());
+
+            List<TradePrivilegeEntity> listPrivilage = mTradePrivilegeService.queryPrivilegeByTradeId(mTradeModel.getBrandIdenty(),mTradeModel.getShopIdenty(),mTradeModel.getTradeId());
+
+            List<TradeUserEntity> listTradeUser = mTradeUserService.queryDataByTradeId(mTradeModel.getTradeId());
+
+            List<TradeItemModel> listTradeItem = new ArrayList<>();
+
+            List<TradePrivilegeEntity> tradePrivilageForTrade = new ArrayList<>();
+
+            TradeUserEntity tradeUserForTrade = new TradeUserEntity();
+
+            Map<Long,TradePrivilegeEntity> privilegeMap = new HashMap<>();
+            Map<Long,TradeUserEntity> tradeUserMap = new HashMap<>();
+
+            for(TradePrivilegeEntity privilage : listPrivilage){
+                if(privilage.getTradeItemId() == null || privilage.getTradeItemId().equals("")){
+                    tradePrivilageForTrade.add(privilage);
+                }else{
+                    privilegeMap.put(privilage.getTradeItemId(),privilage);
+                }
+
+            }
+            for(TradeUserEntity user : listTradeUser){
+                if(user.getTradeItemId() == null || user.getTradeItemId().equals("")){
+                    tradeUserForTrade = user;
+                }else{
+                    tradeUserMap.put(user.getTradeItemId(),user);
+                }
+            }
+
+            for(TradeItemEntity item : listItem){
+                TradeItemModel mTradeItemModel = new TradeItemModel();
+                mTradeItemModel.setDishName(item.getDishName());
+                mTradeItemModel.setPrice(item.getPrice());
+                mTradeItemModel.setQuantity(item.getQuantity());
+                mTradeItemModel.setAmount(item.getAmount());
+                mTradeItemModel.setPrivilege("/");
+                mTradeItemModel.setCreatorName(item.getCreatorName());
+
+                TradePrivilegeEntity mTradePrivilegeEntity = privilegeMap.get(item.getId());
+                if(mTradePrivilegeEntity != null){
+                    mTradeItemModel.setPrivilege(mTradePrivilegeEntity.getPrivilegeName()+"："+mTradePrivilegeEntity.getPrivilegeAmount());
+                }
+
+                mTradeItemModel.setActualAmount(item.getActualAmount());
+                mTradeItemModel.setTradeUser("/");
+
+                TradeUserEntity mTradeUserEntity = tradeUserMap.get(item.getId());
+                if(mTradeUserEntity != null){
+                    mTradeItemModel.setTradeUser(mTradeUserEntity.getUserName());
+                }
+
+                listTradeItem.add(mTradeItemModel);
+            }
+
+
+            List<PaymentItemEntity> listPayment = mPaymentItemService.queryPaymentItemsByTradeId(mTradeEntity.getId());
+
+
+            List<TradeCustomerEntity> listTradeCustomer = mTradeCustomerService.queryTradeCustomerList(mTradeModel.getTradeId());
+
+            model.addAttribute("listTradeCustomer", listTradeCustomer);
+
+            model.addAttribute("listPayment", listPayment);
+
+            model.addAttribute("listItem", listTradeItem);
+
+            model.addAttribute("privilageForTrade", tradePrivilageForTrade);
+            model.addAttribute("userForTrade", tradeUserForTrade);
+
+            model.addAttribute("mTradeModel", mTradeModel);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return "fail";
+        }
+
+        return "brand_trade_detail";
+    }
+
+    /**
+     * 查询订单退款结果
+     * @param model
+     * @param mTradeModel
+     * @return
+     */
+    @RequestMapping("/refundquery")
+    public String refundquery(Model model, TradeModel mTradeModel){
+        try{
+            PaymentItemEntity mPaymentItemEntity = mPaymentItemService.queryPaymentItemById(mTradeModel.getPaymentItemId());
+            mPaymentItemEntity = mPaymentItemService.refundquery(mPaymentItemEntity, mTradeModel.getTradeId());
+
+            return redirect("/internal/brand/trade/tradeDetail?tradeId="+mTradeModel.getTradeId()+"&brandIdenty="+mPaymentItemEntity.getBrandIdenty()+"&shopIdenty="+mPaymentItemEntity.getShopIdenty());
+        }catch (Exception e){
+            e.printStackTrace();
+            return "fail";
+        }
+
+    }
+
+    @RequestMapping("/refundTrade")
+    public String refundTrade(Model model, TradeModel mTradeModel){
+
+        try{
+            PaymentItemEntity oldPaymentItem = mPaymentItemService.queryPaymentItemByTradeId(mTradeModel.getRelationTradeId());
+
+            PaymentItemEntity mPaymentItemEntity = mPaymentItemService.queryPaymentItemById(mTradeModel.getPaymentItemId());
+
+            mPaymentItemService.retrunPayment(oldPaymentItem.getId(),mPaymentItemEntity,mTradeModel.getTradeId());
+
+            return redirect("/internal/brand/trade/tradeDetail?tradeId="+mTradeModel.getTradeId()+"&brandIdenty="+mPaymentItemEntity.getBrandIdenty()+"&shopIdenty="+mPaymentItemEntity.getShopIdenty());
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return "fail";
+
+        }
+
+    }
+}
+
